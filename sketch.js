024fed07e1c943e0b341deb76de18d351ea58dd7
Mr-SkadyYT <66365324+Mr-SkadyYT@users.noreply.github.com>
2020-06-08 21:32:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,15 +14,18 @@ function draw() {
 
   movingRect.y = mouseY;
   movingRect.x = mouseX;
-
- if (movingRect.x - fixedRect.x<fixedRect.width/2 + movingRect.width/2)  {
+ 
+if ((movingRect.x - fixedRect.x<fixedRect.width/2 + movingRect.width/2 )
+    && (fixedRect.x-movingRect.x<fixedRect.width/2 + movingRect.width/2)
+    && (fixedRect.y-movingRect.y<fixedRect.height/2 + movingRect.height/2 )
+    && (movingRect.y-fixedRect.y<fixedRect.height/2 + movingRect.height/2 ) ) {
   movingRect.shapeColor = "blue";
   fixedRect.shapeColor = "blue";
-}
+
 else {fixedRect.shapeColor = "yellow";
-     movingRect.shapeColor = "yellow";
-     
+     movingRect.shapeColor = "yellow";  
  }
+
    
   drawSprites();
  }
