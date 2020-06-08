1a1db4548251d09ff4f630353f790a98c5a724e0
Mr-SkadyYT <66365324+Mr-SkadyYT@users.noreply.github.com>
2020-06-08 21:35:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ if ((movingRect.x - fixedRect.x<fixedRect.width/2 + movingRect.width/2 )
     && (movingRect.y-fixedRect.y<fixedRect.height/2 + movingRect.height/2 ) ) {
   movingRect.shapeColor = "blue";
   fixedRect.shapeColor = "blue";
-
+}
 else {fixedRect.shapeColor = "yellow";
      movingRect.shapeColor = "yellow";  
  }
